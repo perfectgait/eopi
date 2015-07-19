43672a71d654b799c4f917d1510320b4a64dd027
Matt Rathbun <mrathbun80@gmail.com>
2015-07-19 16:34:54 -0500

    6.7 - Variant to find longest sub-array of integers

--- 6.7-buy_sell_stock_once/buy_sell_stock_once.py
+++ 6.7-buy_sell_stock_once/buy_sell_stock_once.py
@@ -30,7 +30,40 @@ def maximum_profit(prices):
     return max_profit
 
 
+def longest_subarray(integers):
+    """
+    Find the length of the longest sub-array where all entries are equal
+
+    i.e. [1,2,3,4,4,4,5,5,6,7,8,9] would be 3
+    """
+    current = 0
+    longest = 0
+    current_value = None
+
+    for integer in integers:
+        if current_value is None or current_value != integer:
+            if current > longest:
+                longest = current
+
+            current = 0
+            current_value = integer
+
+        current += 1
+
+    if current > longest:
+        longest = current
+
+    return longest
+
+
 prices = [310, 315, 275, 295, 260, 270, 290, 230, 255, 250]
 prices2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 prices3 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 print 'The maximum profit to be made from the prices %s is: %d' % (prices, maximum_profit(prices))
+
+integer_array = [1, 2, 3, 4, 4, 4, 5, 5, 6, 7, 8, 9]
+integer_array2 = [1, 2, 2, 2, 2, 2, 1, 1, 1, 1]
+integer_array3 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+integer_array4 = []
+print 'The maximum sub-array where all entries are equal for the integers %s is: %d'\
+      % (integer_array4, longest_subarray(integer_array4))
