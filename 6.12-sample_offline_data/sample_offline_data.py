752895b447c4317e797e03456e2de9379c410bb3
Matt Rathbun <mrathbun80@gmail.com>
2015-07-19 21:35:47 -0700

    6.12 - Initial work

---
+++ 6.12-sample_offline_data/sample_offline_data.py
@@ -0,0 +1,35 @@
+"""
+Compute a sub-set of n elements where each sub-set is equally likely to occur.  This uses the numpy library
+random_integer method to ensure that the random numbers are drawn from a uniform distribution.
+"""
+
+import numpy as np
+
+__author__ = "Matt Rathbun"
+__email__ = "mrathbun80@gmail.com"
+__version__ = "1.0"
+
+
+def compute_subset(array, length):
+    """
+    Compute a sub-set of the array of the specified length where all possible sub-sets are equally likely.
+    """
+    if length > len(array):
+        length = len(array)
+
+    for i in range(0, length):
+        random_number = np.random.random_integers(i, len(array) - 1)
+        array[i], array[random_number] = array[random_number], array[i]
+
+    return array[0:length]
+
+
+original_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+array = list(original_array)
+length = 3
+print 'Computed sub-set %s of length %d from the set %s' % (compute_subset(array, length), length, original_array)
+
+original_array2 = [1, 1, 1, 1, 1]
+array2 = list(original_array2)
+length2 = 5
+print 'Computed sub-set %s of length %d from the set %s' % (compute_subset(array2, length2), length2, original_array2)
