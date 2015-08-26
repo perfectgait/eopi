618731780f0786c6dfd3e4bd57827f81fedc03a0
Matthew Rathbun <mrathbun80@gmail.com>
2015-08-26 15:05:27 -0700

    [PHP] 5.5 - Added explaination for multiplication

--- php/5.5-compute_x*y_without_multiply_or_add/compute_x*y_without_multiply_or_add.php
+++ php/5.5-compute_x*y_without_multiply_or_add/compute_x*y_without_multiply_or_add.php
@@ -11,6 +11,29 @@ use EOPI\Helper\InputHelper;
 /**
  * Compute multiplication without using built-in multiplication
  *
+ * This works by going through each bit of $number1.  When a bit is toggled then the sum is increased by 2^k*$number2
+ * (this is achieved by shifting $number to the left 1 bit in each iteration effectively multiplying it by 2)
+ * i.e.
+ * 110 (6) * 11 (3) = 10010 (18)
+ *
+ * Iteration 1:
+ * First bit of 110 is not set so sum is 0
+ * $number1 = 11 (3)
+ * $number2 = 110 (6)
+ * $sum = 0
+ *
+ * Iteration 2:
+ * First bit of 11 is set so sum is increased by 6
+ * $number1 = 1 (1)
+ * $number2 = 1100 (12)
+ * $sum = 6
+ *
+ * Iteration 3:
+ * First bit of 1 is set so sum is increased by 12
+ * $number1 = 0 (0)
+ * $number2 = 11000 (24)
+ * $sum = 18
+ *
  * @param int $number1
  * @param int $number2
  * @return int
@@ -37,13 +60,24 @@ function computeMultiplicationWithoutMultiplyOrAdd($number1, $number2)
     print PHP_EOL;
     $sum = 0;
 
+//    printf('%b(%d) %b(%d)', $number1, $number1, $number2, $number2);
+//    print PHP_EOL;
+
     while ($number1) {
+        printf('%b(%d) %b(%d)', $number1, $number1, $number2, $number2);
+
         if ($number1 & 1) {
             $sum = computeAdditionWithoutAdd($sum, $number2);
+//            print $number2 . ' + ';
+            print ' = ' . $sum . PHP_EOL;
+        } else {
+            print PHP_EOL;
         }
 
         $number1 >>= 1;
+        // Multiply $number2 by 2
         $number2 <<= 1;
+//        print PHP_EOL;
     }
 
     return $sum;
