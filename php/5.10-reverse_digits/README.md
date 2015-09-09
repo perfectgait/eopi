fa1bc52ce7fbcf5597d1d53aca93ad50722e2bb9
Matthew Rathbun <mrathbun80@gmail.com>
2015-09-09 16:19:34 -0700

    [PHP] 5.10 - Reverse digits

---
+++ php/5.10-reverse_digits/README.md
@@ -0,0 +1,5 @@
+#5.10 Reverse Digits
+This problem is concerned with computing the reverse of the decimal representation of a binary-encoded integer.  For
+example, the reverse of 42 is 24, and the reverse of -314 is -413.
+Write a program which takes an integer K and returns the integer corresponding to the digits of K written in reverse
+order.
---
+++ php/5.10-reverse_digits/reverse_digits.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once '../bootstrap.php';
+
+use EOPI\Helper\InputHelper;
+
+/**
+ * The time complexity is O(n) where n is the number of digits in K
+ */
+
+/**
+ * Reverse the digits in a number.  This works by taking the remainder of the number / 10 and adding that to the new
+ * number.  Then the new number is multiplied by 10 to move the value to the left by 1.  The original number is then
+ * divided by 10.  This happens until the original number is <= 0.
+ * i.e.
+ * 12345
+ *
+ * Iteration 1:
+ * $result = 5 (0 * 10 + 12345 % 10)
+ * $number = 1234.5
+ *
+ * Iteration 2:
+ * $result =
+ *
+ * @param int $number
+ * @return int
+ * @throws \InvalidArgumentException
+ */
+function reverseDigits($number)
+{
+    if (!is_int($number)) {
+        throw new \InvalidArgumentException('$number must be an integer');
+    }
+
+    $isNegative = $number < 0;
+    $number = abs($number);
+    $result = 0;
+
+    while ($number >= 1) {
+        $result = $result * 10 + $number % 10;
+        // Known issue, working with very large numbers will cause issues.  i.e. PHP_INT_MAX
+        $number = ($number / 10);
+    }
+
+    if ($isNegative) {
+        $result *= -1;
+    }
+
+    return $result;
+}
+
+$inputHelper = new InputHelper();
+$number = $inputHelper->readInputFromStdIn('Enter the integer to reverse: ');
+$result = reverseDigits((int)$number);
+printf('The result of reversing the digits in %d is: %d', (int)$number, $result);
+print PHP_EOL;
