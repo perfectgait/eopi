7d9c923c468b26f01dc91bf771ebeef9cd2af53e
Matt Rathbun <mrathbun80@gmail.com>
2015-11-12 17:55:55 -0800

    [PHP] 5.12 - Fully documented

--- php/5.12-uniform_random_numbers/generate_uniform_random_number.php
+++ php/5.12-uniform_random_numbers/generate_uniform_random_number.php
@@ -9,7 +9,77 @@ use EOPI\Helper\InputHelper;
  */
 
 /**
- * Generate a uniform random number between $start and $end inclusive.  This works by...
+ * Generate a uniform random number between $start and $end inclusive.  This works by appending one more bit to the
+ * $result as long as the $result is less than the $range.  Once this finishes the $result is checked to make sure it is
+ * not larger than the $range.  If it is the same process continues until a $result is achieved that is less than the
+ * $range.  Once that happens the $result is added to the $start and the final result is returned.
+ *
+ * If the start is 50 and the end is 75:
+ * $range = 26
+ *
+ * Iteration 1a:
+ * $i = 0
+ * 1 << $i = 1
+ * $result = 1 (binary) (chosen randomly)
+ *
+ * Iteration 2a:
+ * $i = 1
+ * 1 << $1 = 2
+ * $result = 11 (binary) (chosen randomly)
+ *
+ * Iteration 3a:
+ * $i = 2
+ * 1 << $i = 4
+ * $result = 111 (binary) (chosen randomly)
+ *
+ * Iteration 4a:
+ * $i = 3
+ * 1 << $i = 8
+ * $result = 1111 (binary) (chosen randomly)
+ *
+ * Iteration 5a:
+ * $i = 4
+ * 1 << $i = 16
+ * $result = 11111 (binary) (chosen randomly)
+ *
+ * Iteration 6a:
+ * $1 = 5
+ * 1 << $i = 32 so execution halts
+ *
+ * Is 11111 (31) >= 26?  Yes, start over
+ *
+ * Iteration 1b:
+ * $i = 0
+ * 1 << $i = 1
+ * $result = 0 (binary) (chosen randomly)
+ *
+ * Iteration 2b:
+ * $i = 1
+ * 1 << $1 = 2
+ * $result = 01 (binary) (chosen randomly)
+ *
+ * Iteration 3b:
+ * $i = 2
+ * 1 << $i = 4
+ * $result = 010 (binary) (chosen randomly)
+ *
+ * Iteration 4b:
+ * $i = 3
+ * 1 << $i = 8
+ * $result = 0101 (binary) (chosen randomly)
+ *
+ * Iteration 5b:
+ * $i = 4
+ * 1 << $i = 16
+ * $result = 01010 (binary) (chosen randomly)
+ *
+ * Iteration 6b:
+ * $1 = 5
+ * 1 << $i = 32 so execution halts
+ *
+ * Is 01010 (10) >= 26?  No, we have a good number
+ *
+ * return $result (10) + $start (50) = 60
  *
  * @param int $start
  * @param int $end
@@ -32,16 +102,16 @@ function generateUniformRandomNumber($start, $end)
     $start = (int) $start;
     $end = (int) $end;
 
-    $limit = $end - $start + 1;
+    $range = $end - $start + 1;
 
     do {
         $result = 0;
 
         // Make sure that if 1 << $i is less than zero we stop.  Otherwise we will loop forever
-        for ($i = 0; ((1 << $i) < $limit) && ((1 << $i) > 0); $i++) {
-            $result = ($result * 2) | rand(0, 1);
+        for ($i = 0; ((1 << $i) < $range) && ((1 << $i) > 0); $i++) {
+            $result = ($result << 1) | rand(0, 1);
         }
-    } while ($result >= $limit);
+    } while ($result >= $range);
 
     return $result + $start;
 }
