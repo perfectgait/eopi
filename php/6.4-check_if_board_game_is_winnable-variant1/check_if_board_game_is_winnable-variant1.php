6546d9ff45ca5daa027a472e70bc42e8685202a2
Matt Rathbun <mrathbun80@gmail.com>
2015-12-08 21:01:13 -0800

    [PHP] 6.4 - Check if Board Game is Winnable Variant 1 Proper

--- php/6.4-check_if_board_game_is_winnable-variant1/check_if_board_game_is_winnable-variant1.php
+++ php/6.4-check_if_board_game_is_winnable-variant1/check_if_board_game_is_winnable-variant1.php
@@ -5,120 +5,127 @@ require_once '../bootstrap.php';
 use EOPI\Helper\InputHelper;
 
 /**
- * The time complexity is O(n) where n is the length of the input array
+ * The time complexity is O(n) where n is the length of the board.  If every entry in the board is added as a move it
+ * can only be replaced once.  At most the function will go through the board once and remove every entry once.
+ *
+ * The storage complexity is O(n).  If every entry in the board points to the next the maximum size of the stack becomes
+ * the size of the board.
  */
 
 /**
- * Return the number of moves it takes to win a board game or false if it cannot be won.  This works by going through
- * each index in the board and determining the maximum move that can be made.  The maximum move is the current index +
- * the number at that position in the board (i.e. if the current index is 1 and the number at position 1 is 5 the
- * maximum move is 6).  If maximum move is greater than the current furthest reach, the number of moves is incremented
- * by 1 because that is a move we want to make.  At the end of the loop if the furthest reach is less than the size of
- * the board the game is un-winnable.  If not, the game is winnable and the number of moves needed to win is returned.
+ * Return the number of moves it takes to win a board game or false if it cannot be won.  This works by first adding the
+ * last element in the board to the stack of moves.  Then the function traverses the board in reverse order and at each
+ * iteration, pops all elements off the moves stack that is less efficient than the current move.  After all inefficient
+ * moves have been popped off, the last move and the current move are added to the stack.
  *
  * i.e.
  *
  * If the board is [3,3,1,0,2,0,1]
  *
- * $furthestReach = 0
- * $moves = 0
+ * $moves = [6]
  *
  * <<< LOOP START >>>
  *
  * Iteration 1:
- * $i = 0
- * is $i(0) + $board[$i](3) > $furthestReach(0)?, YES
- * $moves = 1
- * $furthestReach = 3
+ * $i = 5
+ * $i(5) + $board[$i](0) is not >= any moves, nothing happens
+ * $moves = [6]
  *
  * Iteration 2:
- * $i = 1
- * is $i(1) + $board[$i](3) > $furthestReach(3)?, YES
- * $moves = 2
- * $furthestReach = 4
+ * $i = 4
+ * $i(4) + $board[$i](2) is >= than the only move in the stack
+ * $lastMove = 6
+ * $moves = [6, 4]
  *
  * Iteration 3:
- * $i = 2
- * is $i(2) + $board[$i](1) > $furthestReach(4)?, NO
- * $moves = 2
- * $furthestReach = 4
+ * $i = 3
+ * $i(3) + $board[$i](0) is not >= any moves, nothing happens
+ * $moves = [6, 4]
  *
  * Iteration 4:
- * $i = 3
- * is $i(3) + $board[$i](0) > $furthestReach(4)?, NO
- * $moves = 2
- * $furthestReach = 4
+ * $i = 2
+ * $i(2) + $board[$i](1) is not >= any moves, nothing happens
+ * $moves = [6, 4]
  *
  * Iteration 5:
- * $i = 4
- * is $i(4) + $board[$i](2) > $furthestReach(4)?, YES
- * $moves = 3
- * $furthestReach = 6
+ * $i = 1
+ * $i(1) + $board[$i](3) is >= than the top move in the stack
+ * $lastMove = 4
+ * $moves = [6, 4, 1]
  *
- * <<< LOOP TERMINATION: $furthestReach (6) is not < count($board) - 1 >>>
+ * Iteration 6:
+ * $i = 0
+ * $i(0) + $board[$i](3) is >= than the top move in the stack
+ * $lastMove = 1
+ * $moves = [6, 4, 1, 0]
  *
- * Game is winnable in 3 moves
+ * <<< LOOP TERMINATION: $i < 0 >>>
+ *
+ * Popping the stack until it's empty yields the sequence 0-->1-->4-->6
  *
  * ===============================================================
  *
- * If the board is [100,3,1,0,2,0,1]
+ * If the board is [100,2,0,0,2,0,1]
  *
- * $furthestReach = 0
- * $moves = 0
+ * $moves = [6]
  *
  * <<< LOOP START >>>
  *
  * Iteration 1:
- * $i = 0
- * is $i(0) + $board[$i](100) > $furthestReach(0)?, YES
- * $moves = 1
- * $furthestReach = 100
+ * $i = 5
+ * $i(5) + $board[$i](0) is not >= any moves, nothing happens
+ * $moves = [6]
  *
  * Iteration 2:
- * $i = 1
- * is $i(1) + $board[$i](3) > $furthestReach(100)?, NO
- * $moves = 1
- * $furthestReach = 100
+ * $i = 4
+ * $i(4) + $board[$i](2) is >= the only move in the stack
+ * $lastMove = 6
+ * $moves = [6, 4]
  *
  * Iteration 3:
- * $i = 2
- * is $i(2) + $board[$i](1) > $furthestReach(100)?, NO
- * $moves = 1
- * $furthestReach = 100
+ * $i = 3
+ * $i(3) + $board[$i](0) is not >= any moves, nothing happens
+ * $moves = [6, 4]
  *
  * Iteration 4:
- * $i = 3
- * is $i(3) + $board[$i](0) > $furthestReach(100)?, NO
- * $moves = 1
- * $furthestReach = 100
+ * $i = 2
+ * $i(2) + $board[$i](0) is not >= any moves, nothing happens
+ * $moves = [6, 4]
  *
  * Iteration 5:
- * $i = 4
- * is $i(4) + $board[$i](2) > $furthestReach(100)?, NO
- * $moves = 1
- * $furthestReach = 100
+ * $i = 1
+ * $i(1) + $board[$i](2) is not >= any moves, nothing happens
+ * $moves = [6, 4]
  *
- * <<< LOOP TERMINATION: $furthestReach (6) is not < count($board) - 1 >>>
+ * Iteration 6:
+ * $i = 0
+ * $i(0) + $board[$i](100) is >= than every move in the stack
+ * $lastMove = 6
+ * $moves = [6, 0]
  *
- * Game is winnable in 1 move
+ * <<< LOOP TERMINATION: $i < 0 >>>
+ *
+ * Popping the stack until it's empty yields the sequence 0-->6
  *
  * @param array $board
- * @return int|bool
+ * @return \SplStack
  */
 function getNumberOfMovesToWin(array $board)
 {
-    $furthestReach = 0;
-    $moves = 0;
+    $moves = new \SplStack();
+    $moves->push(count($board) - 1);
 
-    for ($i = 0; $i <= $furthestReach && $furthestReach < count($board) - 1; $i++) {
-        if ($i + $board[$i] > $furthestReach) {
-            $moves++;
-            $furthestReach = $i + $board[$i];
+    for ($i = count($board) - 2; $i >= 0; $i--) {
+        $lastMove = null;
+
+        while (!$moves->isEmpty() && $i + $board[$i] >= $moves->top()) {
+            $lastMove = $moves->pop();
         }
-    }
 
-    if ($furthestReach < count($board) - 1) {
-        return false;
+        if ($lastMove) {
+            $moves->push($lastMove);
+            $moves->push(min(count($board) - 1, $i));
+        }
     }
 
     return $moves;
@@ -128,10 +135,28 @@ $inputHelper = new InputHelper();
 $array = json_decode($inputHelper->readInputFromStdIn('Enter the board as an array in json format: '));
 $result = getNumberOfMovesToWin($array);
 
-if ($result) {
-    printf('The board %s can be won in %d move(s)', json_encode($array), $result);
-} else {
+// If the first move does not originate from the beginning of the board, the game cannot be won
+if ($result->top() != 0) {
     printf('The board %s is not winnable', json_encode($array));
+} else {
+    $sequenceString = 'Sequence: ';
+    // Remove one from the count for the last element which is always included
+    $moveCount = $result->count() - 1;
+
+    while (!$result->isEmpty()) {
+        $sequenceString .= $result->pop();
+
+        if (!$result->isEmpty()) {
+            $sequenceString .= '-->';
+        }
+    }
+
+    printf(
+        'The board %s can be won in %d moves with the following sequence of indexes: %s',
+        json_encode($array),
+        $moveCount,
+        $sequenceString
+    );
 }
 
 print PHP_EOL;
